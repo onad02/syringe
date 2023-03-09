87ddf2930dc6233cbfe5cace981e84cc0ea6fefb
Leonard <leonardbiano2@gmail.com>
2023-03-09 14:47:45 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,34 @@ Route::post('register', [App\Http\Controllers\AuthController::class, 'postRegist
 Route::post('logout', [App\Http\Controllers\AuthController::class, 'postLogout'])->name('logout');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//Clear route cache
+ Route::get('/route-cache', function() {
+     \Artisan::call('route:cache');
+     return 'Routes cache cleared';
+ });
+
+ //Clear config cache
+ Route::get('/config-cache', function() {
+     \Artisan::call('config:cache');
+     return 'Config cache cleared';
+ }); 
+
+ // Clear application cache
+ Route::get('/clear-cache', function() {
+     \Artisan::call('cache:clear');
+     return 'Application cache cleared';
+ });
+
+ // Clear view cache
+ Route::get('/view-clear', function() {
+     \Artisan::call('view:clear');
+     return 'View cache cleared';
+ });
+
+ // Clear cache using reoptimized class
+ Route::get('/optimize-clear', function() {
+     \Artisan::call('optimize:clear');
+     return 'View cache cleared';
+ });
