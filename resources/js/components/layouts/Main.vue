812dccb3031ae8bb0c03fdc60c5c2c7c13833a68
Leonard <leonardbiano2@gmail.com>
2023-03-09 22:50:37 +0800

    fix loader

--- resources/js/components/layouts/Main.vue
+++ resources/js/components/layouts/Main.vue
@@ -11,37 +11,21 @@
         <Footer v-show="currentRouteName != 'signup'" v-bind:website_infos="website_infos" v-bind:skills="skills"/>
     </v-app>
 </template>
-
-<script>
-    import {mapActions} from 'vuex'
-    import { useRoute } from 'vue-router';
+<script setup>
+    import { ref, computed } from 'vue'
+    import { useRouter } from 'vue-router';
     import Navbar from '@/components/layouts/Navbar.vue'
     import Footer from '@/components/layouts/Footer.vue'
-    export default {
-        name:"main",
-        components: {
-            Navbar, Footer
-        },
-        data(){
-            return {
-                skills: [],
-                website_infos: []
-            }
-        } ,
-        created(){
-            this.data();
-        },
-        computed:{
-            currentRouteName(){
-               return useRoute().name
-            }
-        },
-        methods:{
-            async data(){
-                const { data } = await axios.get('/api/site-data');
-                this.skills = data.skills;
-              this.website_infos = data.website_infos;
-            }
-        }
-    }
+    
+    const skills = ref([]);
+    const website_infos = ref([]);
+
+    const currentRouteName = computed(() => {
+      return useRouter().currentRoute.value.name;
+    })
+
+    const { data } = await axios.get('/api/site-data');
+    skills.value = data.skills;
+    website_infos.value = data.website_infos;
+
 </script>
